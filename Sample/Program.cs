f28a3517096dddba0be5dc390774549de1ec5bc6
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2021-10-30 14:03:07 +0530

    adding result type

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,20 +1,30 @@
 ﻿using System.Runtime.CompilerServices;
 
-LruCache<int> cache = new();
-cache.Refer(1);
-cache.Print();
-cache.Refer(2);
-cache.Print();
-cache.Refer(3);
-cache.Print();
-cache.Refer(4);
-cache.Print();
-cache.Refer(7);
-cache.Print();
 
-public class LruCache<T>
+LruCache<int,int> cache = new(100);
+Console.WriteLine(Fibbo(10));
+
+int Fibbo(int n)
+{
+    var contains=cache.Refer(n);
+
+    if(contains)
+        return cache.Get(n);
+
+    if (n == 1 || n==0)
+        return n;
+
+    var v= Fibbo(n - 1)+Fibbo(n-2);
+
+    cache.AddResult(n, v);
+
+    return v;
+}
+
+public class LruCache<T,R>
 {
     private Dictionary<T,DoublyNode<T>> cache = new();
+    private Dictionary<T, R> result = new();
 
     private DoublyLinkList<T> list=new();
     public LruCache(int capacity=3)
@@ -23,42 +33,85 @@ public class LruCache<T>
     }
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void Print()=>list.Print();
-
     public int Capacity { get; }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void Refer(T value)
+    public bool Refer(T value)
     {
+        //if we have page hit
         if(cache.ContainsKey(value))
         {
+            //if hit locaton is not in front we do work
             if(cache[value]!=list.Front)
             {
+                //get previous and next nodes
                 var previous=cache[value].Previous;
                 var next=cache[value].Next;
 
+                //delete current node and reassign pointers
                 if(previous!=null)
                     previous.Next=next;
                 if(next!=null)
                     next.Previous=previous;
 
+                //manually adjust the count
                 list.Count--;
+
+                //remove value from cache
                 cache.Remove(value);
+
+                //create new node add it in last and push to cache new pointer
                 var node = list.Push(value);
                 cache.Add(value, node);
             }
+            return true;
         }
+        //if capacity is full
         else if (list.Count == Capacity)
         {
+            //get node in front which will be deleted
+            var last = list.Front;
+
+            //remove result of last node 
+            if (last != null)
+                result.Remove(last.Value);
+
+            //remove the node
             list.Pop();
+
+            //add new node
             var node = list.Push(value);
             cache.Add(value, node);
         }
+        //can accomodate more node , add new node
         else
         {
+            //add new node
             var node = list.Push(value);
             cache.Add(value, node);
         }
 
+        return false;
+
+    }
+    public R Get(T value)
+    {
+        if(result.ContainsKey(value))
+            return result[value];
+        return default;
+    }
+    public bool ContainsResult(T input)
+    {
+        return result.ContainsKey(input);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void AddResult(T input,R output)
+    {
+        if(result.ContainsKey(input))
+            result[input] = output;
+        else
+            result.Add(input, output);
     }
 }
 
@@ -118,9 +171,6 @@ internal class DoublyNode<T>
     public DoublyNode<T> Next { get; set; } = null;
     public DoublyNode<T> Previous { get; set; }= null;
     public T Value { get; set; }
-    public DoublyNode(T value)
-    {
-        Value = value;
-    }
+    public DoublyNode(T value) => Value = value;
 
 }
